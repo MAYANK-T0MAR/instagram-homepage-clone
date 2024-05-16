3bad4472281b8be3a8db4694d242a9f90b6f409b
Mayank Tomar <137810128+MAYANK-T0MAR@users.noreply.github.com>
2024-05-16 22:43:06 +0530

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,61 @@
+const arr = [
+    {
+     username:"user1",
+     pfp:"https://i.pinimg.com/564x/6b/2c/0b/6b2c0bd51b529791a7381f1e490bc2d6.jpg",
+     story: "https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     post: "https://images.pexels.com/photos/601174/pexels-photo-601174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 52
+    },
+    {
+     username:"user2",
+     pfp:"https://i.pinimg.com/564x/a1/81/97/a181977a2d6a19d37f48c843fbba991d.jpg",
+     story: "https://images.pexels.com/photos/1624438/pexels-photo-1624438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     post: "https://images.pexels.com/photos/755858/pexels-photo-755858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 23
+    },
+    {
+     username:"user3",
+     pfp:"https://i.pinimg.com/564x/7a/9c/c3/7a9cc36f478969da92e2e9fee391a5b2.jpg",
+     story: "https://i.pinimg.com/564x/88/7d/13/887d133d28b5a6ee71a4f15e60fe2513.jpg",
+     post: "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 17
+    },
+    {
+     username:"user4",
+     pfp:"https://i.pinimg.com/564x/e8/4a/46/e84a463624e98ca3f4a4023b4c0f11e2.jpg",
+     story: "https://i.pinimg.com/564x/5a/56/3c/5a563cc4addd581138da2f85ec018ee9.jpg",
+     post: "https://images.pexels.com/photos/242261/pexels-photo-242261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 65
+    },
+    {
+     username:"user5",
+     pfp:"https://i.pinimg.com/564x/d3/bb/2b/d3bb2bfe476594029148a2430503e51c.jpg",
+     story: "https://i.pinimg.com/564x/d9/a7/a4/d9a7a43f302666700779f7ee3731d5b4.jpg",
+     post: "https://images.pexels.com/photos/1006117/pexels-photo-1006117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 13
+    },
+    {
+     username:"user6",
+     pfp:"https://i.pinimg.com/564x/bb/1a/89/bb1a89b7cc6c852725b7604d447ca999.jpg",
+     story: "https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     post: "https://images.pexels.com/photos/248195/pexels-photo-248195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 39
+    },
+    {
+     username:"user7",
+     pfp:"https://i.pinimg.com/564x/dd/af/be/ddafbecd3f250a6dcbdb7ae4670035ec.jpg",
+     story: "https://images.pexels.com/photos/1905054/pexels-photo-1905054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     post: "https://images.pexels.com/photos/757240/pexels-photo-757240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 27
+    },
+    {
+     username:"user8",
+     pfp:"https://i.pinimg.com/564x/ce/c2/50/cec25097f0ad911e750de8addbe1b9b3.jpg",
+     story: "https://i.pinimg.com/564x/24/c6/e1/24c6e19582040ea8ba65fc50e49e542c.jpg",
+     post: "https://images.pexels.com/photos/1423591/pexels-photo-1423591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     likes: 31
+    }
+]
+
+
+export{arr};
---
+++ script.js
@@ -0,0 +1,178 @@
+import {arr} from './contents.js';
+const storySection = document.querySelector("#story-section")
+const storyContent = document.querySelector(".story-content")
+const container = document.querySelector(".container")
+const menu = document.querySelector("#menu")
+const story = document.querySelector(".story")
+const isLiked = [];
+
+var data = ""
+var postData = ""
+arr.forEach(function(elem, i) {
+    data += `
+            <div class="single-story">
+                <div class="story">
+                    <img id="${i}" src="${elem.pfp}" alt="pfp">
+                </div>
+                <div class="namesBelow">
+                    ${elem.username}
+                </div>
+            </div>
+            `
+    postData += `
+    <div class="post">
+    <div class="post-name">
+        <div class="post-pfp">
+            <img id="${i}" src="${elem.pfp}" alt="post">
+        </div>
+        <div class="post-username">${elem.username}</div>
+        <div class="post-options">
+            <img src="icons/more.png" alt="">
+        </div>
+    </div>
+    <div class="post-image" style="background-image: url('${elem.post}')">
+        <div class="heart-popup"></div>
+    </div>
+    <div class="post-actions">
+        <div class="social">
+            <div class="like">
+                <img src="icons/heart.png" alt="">
+            </div>
+            <div class="comment">
+                <img src="icons/chat (1).png" alt="">
+            </div>
+            <div class="send">
+                <img src="icons/direct-instagram.png" alt="">
+            </div>
+        </div>
+        <div class="save">
+            <img src="icons/bookmark (2).png" alt="">
+        </div>
+    </div>
+    <div class="info">
+        <span class="text">${elem.likes} likes <br>${elem.username}</span>
+        <span class="coments">view all 5 comments</span>
+    </div>
+</div>
+    `
+});
+
+
+storySection.innerHTML = data; //to insert stories data in the story section div
+menu.insertAdjacentHTML('beforebegin', postData);  //to insert post data in the container div just before menu (as menu is a static div so it'll be there just after the story section div before the post data arrives and we have to make sure that the menu div always sticks to the bottom of the container and the post data goes between story section div and menu div hence insertAdjacentHTML(beforebegin))
+storySection.addEventListener("click", function(dets){
+    viewStory(dets); //view story function in story section (viewstory function defined below)
+    dets.target.parentElement.style.background = "#a3a3a3";    
+    const postImage = container.querySelector(`.post img[id='${dets.target.id}']`);
+    postImage.parentElement.style.background = "none";
+    postImage.parentElement.style.padding = "0";
+    
+    
+})
+
+const postName = document.querySelectorAll(".post-name");
+
+postName.forEach(function(elem){
+    elem.addEventListener("click", function(dets){
+        viewStory(dets); //view story function in post section div where the user pfp displays (viewstory function defined below)
+        dets.target.parentElement.style.background = "none";
+        dets.target.parentElement.style.padding = "0";
+        var imageId = dets.target.id;
+        storySection.querySelector(`img[id='${imageId}']`).parentElement.style.background = "#a3a3a3";
+    })
+})
+
+
+
+//---------------------double tap to like functionality--------------------
+const postImages = document.querySelectorAll(".post-image");
+const heartPopups = document.querySelectorAll(".heart-popup");
+const likeButtons = document.querySelectorAll(".like");
+const likes = document.querySelectorAll(".info");
+
+postImages.forEach(function(postImage, index){
+    isLiked[index] = false;
+    postImage.addEventListener("dblclick", function(){
+        const heartPopup = heartPopups[index];
+        const particularLikeButton = likeButtons[index];
+        // Add the animation class
+        heartPopup.classList.add("animate-heart");
+        particularLikeButton.querySelector("img").src = "icons/heartRed.png";
+        
+        if(!isLiked[index]){
+            arr[index].likes++;
+            const like = likes[index]
+            like.innerHTML =  `<span class="text">${arr[index].likes} likes <br>${arr[index].username}</span>
+                            <span class="coments">view all 5 comments</span>`
+        }
+        isLiked[index] = true;
+
+        // After a delay, remove the animation class to enable re-triggering
+        setTimeout(function() {
+            heartPopup.classList.remove("animate-heart");
+        }, 1000);
+    });
+});
+//---------------------double tap to like functionality--------------------
+
+
+
+
+
+//---------------------Tap on the like button to like functionality--------------------
+document.addEventListener("DOMContentLoaded", function() {
+    const likeButtons = document.querySelectorAll(".like");
+
+    likeButtons.forEach(function(particularLikeButton, index) {
+        // let isLiked = false;
+        const like = likes[index];
+        particularLikeButton.addEventListener("click", function() {
+            const image = particularLikeButton.querySelector("img");
+            if (!isLiked[index]) {
+                image.src = "icons/heartRed.png";
+                arr[index].likes++;
+                like.innerHTML =  `<span class="text">${arr[index].likes} likes <br>${arr[index].username}</span>
+                                    <span class="coments">view all 5 comments</span>`
+            } else {
+                image.src = "icons/heart.png";
+                arr[index].likes--;
+                like.innerHTML =  `<span class="text">${arr[index].likes} likes <br>${arr[index].username}</span>
+                                    <span class="coments">view all 5 comments</span>`
+
+            }
+            isLiked[index] = !isLiked[index]; // Toggle the state
+        });
+    });
+});
+//---------------------Tap on the like button to like functionality--------------------
+
+
+
+//---------------------function to view stories--------------------
+function viewStory(dets){
+    storyContent.style.backgroundImage = `url(${arr[dets.target.id].story})`
+    document.querySelector(".pfp").style.backgroundImage = `url(${arr[dets.target.id].pfp})`
+    document.querySelector("#inside-story-name").innerHTML = arr[dets.target.id].username
+    storyContent.style.display = "flex"
+    container.style.overflowY = "hidden"
+    container.style.padding = "0"
+
+    var timeoutID = setTimeout(function(){
+        storyContent.style.display = "none"
+        container.style.overflowY = "auto"
+    }, 8000);
+
+
+
+    document.querySelector("#cross").addEventListener("click", function(){
+        clearTimeout(timeoutID)
+        storyContent.style.display = "none"
+        container.style.overflowY = "auto"
+    });
+
+
+}
+//---------------------function to view stories--------------------
+
+
+
